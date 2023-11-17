adebfed4a1c5feb26fd501e095f30042c8333aaa
Sergio Amo <sdarillo@gmail.com>
2023-11-17 18:56:17 +0100

    Initial Commit

---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by Sergio Amo on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
