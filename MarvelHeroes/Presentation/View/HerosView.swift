164f895a472b29f52f2d1b4a299608bd3b4d7443
Sergio Amo <sdarillo@gmail.com>
2023-11-18 16:33:01 +0100

    Add rootViewModel and rootview (status box/ ViewRouter)

---
+++ MarvelHeroes/Presentation/View/HerosView.swift
@@ -0,0 +1,18 @@
+//
+//  HerosView.swift
+//  MarvelHeroes
+//
+//  Created by Sergio Amo on 18/11/23.
+//
+
+import SwiftUI
+
+struct HerosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HerosView()
+}
