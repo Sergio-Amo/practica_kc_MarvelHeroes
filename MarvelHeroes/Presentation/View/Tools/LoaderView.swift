819bfc86224baaacf4215322d39559c57aacab0f
Sergio Amo <sdarillo@gmail.com>
2023-11-18 17:21:55 +0100

    Add loadingView

---
+++ MarvelHeroes/Presentation/View/Tools/LoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  LoaderView.swift
+//  MarvelHeroes
+//
+//  Created by Sergio Amo on 18/11/23.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoaderView()
+}
