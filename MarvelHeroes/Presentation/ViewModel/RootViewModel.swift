7162a3f64423236cc1864d13277a811fe01bf22a
Sergio Amo <sdarillo@gmail.com>
2023-11-18 18:31:55 +0100

    Add quick and dirty getHeros mock for testing and design.

--- MarvelHeroes/Presentation/ViewModel/RootViewModel.swift
+++ MarvelHeroes/Presentation/ViewModel/RootViewModel.swift
@@ -39,4 +39,28 @@ final class RootViewModel: ObservableObject {
             }
             .store(in: &suscriptors)
     }
+    
+    
+    // TODO: move this to network testing
+    
+    func getHerosTesting(){
+        self.status = .loading
+        self.marvel =  getHerosDesign()
+        self.status = .loaded
+    }
+    
+    func getHerosDesign() -> Marvel {
+        let heroes = [
+            Hero(id: 1, name: "Foo", description: "LoremIpsum", 
+                 thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784", thumbnailExtension: "jpg")),
+            Hero(id: 2, name: "Bar", description: "LoremIpsum",
+                 thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16", thumbnailExtension: "jpg")),
+            Hero(id: 3, name: "Baz", description: "LoremIpsum",
+                 thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/6/20/52602f21f29ec", thumbnailExtension: "jpg")),
+            Hero(id: 4, name: "FooBar", description: "LoremIpsum",
+                 thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available", thumbnailExtension: "jpg"))
+        ]
+        return Marvel(code: 200, data: DataClass(total: 20, offset: 0, count: 4, results: heroes))
+    }
 }
+
